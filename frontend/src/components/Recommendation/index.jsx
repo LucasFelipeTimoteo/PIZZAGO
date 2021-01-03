8d98f527a5a74477516b476009111ddf2791c484
Lucas Felipe <conhecimento54321@gmail.com>
2021-01-03 17:03:38 -0300

    Replace 'Typography' title to 'Titles' component ♻️

--- frontend/src/components/Recommendation/index.jsx
+++ frontend/src/components/Recommendation/index.jsx
@@ -1,28 +1,15 @@
 import React, { Suspense, useState } from 'react'
-import sampleImage from '../../assets/images/sample-bg.jpg'
-import { cardAltDescription, cardAltName } from '../../alternativeTexts/altTexts'
 
-import {
-  Card,
-  CardActionArea,
-  CardContent,
-  CardMedia,
-  Grid,
-  Typography
-} from '@material-ui/core';
-
+import Titles from '../Titles';
+import Cards from '../Cards';
 import NavButtons from '../NavButtons'
+import ConfirmModalBox from '../ConfirmModalBox';
 
 import useRecommendation from '../../hooks/useRecommendation';
-import useCardStyles from '../../styles/components/contentCard';
-import useTitleStyles from '../../styles/components/titlles';
-import ConfirmModalBox from '../ConfirmModalBox';
 
 export default function Recommendation({ step }) {
   const [isOpenModalBox, setIsOpenModalBox] = useState(false)
 
-  const cardClasses = useCardStyles()
-  const titleClasses = useTitleStyles()
   const recommendation = useRecommendation()
 
   const handleConfirmation = () => {
@@ -35,36 +22,8 @@ export default function Recommendation({ step }) {
   return (
     <>
       <Suspense fallback={<p>Loading data...</p>}>
-        <Typography variant="h3" component="h2" className={titleClasses.title}>
-          recomendação
-        </Typography>
-        <Grid container className={cardClasses.cardGroup} justify="center">
-
-          {
-            recommendation.map((dayRecommendation, index) => (
-              <Grid item key={dayRecommendation.name || index}>
-                <Card className={cardClasses.card}>
-                  <CardActionArea disabled>
-                    <CardMedia
-                      className={cardClasses.cardMedia}
-                      image={dayRecommendation.image || sampleImage}
-                      title={`massa ${dayRecommendation.name}`}
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {dayRecommendation.name || cardAltName}
-                      </Typography>
-                      <Typography variant="body2" color="textSecondary" component="p">
-                        {dayRecommendation.description || cardAltDescription}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-              </Grid>
-            ))
-          }
-
-        </Grid>
+        <Titles componentName="Recommendation" />
+        <Cards pizzaOptions={recommendation} />
         <NavButtons step={step} handleConfirmation={handleConfirmation} />
         <ConfirmModalBox isOpenModalBox={isOpenModalBox} isRecommendation />
       </Suspense>

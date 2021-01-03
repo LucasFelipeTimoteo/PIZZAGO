b0a00f532350ec67ed0484aa0857fbf918656294
Lucas Felipe <conhecimento54321@gmail.com>
2021-01-03 17:03:27 -0300

    Replace 'Typography' title to 'Titles' component ♻️

--- frontend/src/components/Options/index.jsx
+++ frontend/src/components/Options/index.jsx
@@ -1,27 +1,15 @@
 import React, { Suspense } from 'react'
-import sampleImage from '../../assets/images/sample-bg.jpg'
-import { cardAltDescription, cardAltName } from '../../alternativeTexts/altTexts'
 
-import {
-  Card,
-  CardActionArea,
-  CardContent,
-  CardMedia,
-  Grid,
-  Typography
-} from '@material-ui/core';
-
-import useOptions from '../../hooks/useOptions';
-import useCardStyles from '../../styles/components/contentCard';
-import useTitleStyles from '../../styles/components/titlles';
+import Titles from '../Titles';
+import Cards from '../Cards';
 import NavButtons from '../NavButtons';
 
+import useOptions from '../../hooks/useOptions';
+
 export default function Options({ step, nextStep, recommendationStep }) {
-  const cardClasses = useCardStyles()
-  const titleClasses = useTitleStyles()
   const options = useOptions()
 
-  const handleCardClick = (name) => {
+  const handleOptionsCardClick = (name) => {
     if(name === 'montar') {
       nextStep()
     }
@@ -36,39 +24,8 @@ export default function Options({ step, nextStep, recommendationStep }) {
   return (
     <>
       <Suspense fallback={<p>Loading data...</p>}>
-        <Typography variant="h3" component="h2" className={titleClasses.title}>
-          opções
-        </Typography>
-        <Grid container className={cardClasses.cardGroup} justify="center">
-
-          {
-            options.map((option, index) => (
-              <Grid item
-                key={option.name || index}
-                onClick={ () => handleCardClick(option.name) }
-              >
-                <Card className={cardClasses.card}>
-                  <CardActionArea>
-                    <CardMedia
-                      className={cardClasses.cardMedia}
-                      image={option.image || sampleImage}
-                      title={`massa ${option.name}`}
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {option.name || cardAltName}
-                      </Typography>
-                      <Typography variant="body2" color="textSecondary" component="p">
-                        {option.description || cardAltDescription}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-              </Grid>
-            ))
-          }
-
-        </Grid>
+        <Titles componentName="Options" />
+        <Cards pizzaOptions={options} handleOptionsOfOptionsComponent={handleOptionsCardClick} />
         <NavButtons step={step} />
       </Suspense>
     </>

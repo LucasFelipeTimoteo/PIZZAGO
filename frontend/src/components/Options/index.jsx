7490b5644452ee887564bc7dc52d53f7f15fe1c4
Lucas Felipe <conhecimento54321@gmail.com>
2021-01-03 17:34:33 -0300

    Update import ⬆️

--- frontend/src/components/Options/index.jsx
+++ frontend/src/components/Options/index.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from 'react'
 
-import Titles from '../Titles';
+import Titles from '../../parts/Titles';
 import Cards from '../Cards';
 import NavButtons from '../NavButtons';
 

4724efa813e2228730bb481f26144e25d24a5f4b
Lucas Felipe <conhecimento54321@gmail.com>
2021-01-10 22:55:17 -0300

    Add responsive fontSizes

--- frontend/src/styles/theme/MuiTheme.js
+++ frontend/src/styles/theme/MuiTheme.js
@@ -1,7 +1,7 @@
-import { createMuiTheme } from "@material-ui/core";
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core";
 import { orange } from "@material-ui/core/colors";
 
-const MuiTheme = createMuiTheme({
+let MuiTheme = createMuiTheme({
   palette: {
     primary: {
       main: orange[500]
@@ -10,5 +10,6 @@ const MuiTheme = createMuiTheme({
   }
 })
 
-export default MuiTheme
+MuiTheme = responsiveFontSizes(MuiTheme);
 
+export default MuiTheme
